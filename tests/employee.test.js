74796a7587a1051e47fb1034ec7e666b361773ac
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-03 11:35:55 -0500

    building test functions for employee js

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,14 @@
+const { expect } = require("@jest/globals");
+const { test } = require("jest-circus");
 const employee = require("../lib/employee");
+
+test("Can instantiate Employee instance", () => {
+    const e = new employee();
+    expect(typeof(e)).toBe("object");
+});
+
+test("Can set name via constructor arguments", () => {
+    const name = "Eduard";
+    const e = new employee(name);
+    expect(e.name).toBe(name);
+});
