9c3be6e7af94c8569a13174fd799f59256c3ca14
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-04 15:26:44 -0500

    finished manager test file

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -8,3 +8,15 @@ test("Can set office number via constructor argument", () => {
     const e = new manager("Coffee", 1, "test@test.com", testValue);
     expect(e.officeNumber).toBe(testValue);
 });
+
+test('getRole() should return "Manager"', () => {
+    const testValue = "Manager";
+    const e = new manager("Coffee", 1, "test@test.com", 100, testValue);
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+    const testValue = 100;
+    const e = new manager("Coffee", 1, "test@test.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
