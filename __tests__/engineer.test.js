7c124dc3ad6a0f79807855d4e2efeb092ed7b3f3
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-04 15:52:43 -0500

    finished engineer test file

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -7,3 +7,15 @@ test("Can set GitHub account via constructor", () => {
     const e = new engineer("Coffee", 1, "test@test.com", testValue);
     expect(e.github).toBe(testValue);
 });
+
+test("getRole() should return \"engineer\"", () => {
+    const testValue = "engineer";
+    const e = new engineer("Coffee", 1, "test@test.com", "GitHubAccount");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGitHub()", () => {
+    const testValue = "GitHubAccount";
+    const e = new engineer("Coffee", 1, "test@test.com", testValue);
+    expect(e.getGitHub()).toBe(testValue);
+});
