021a3e88c82467f2dfac8ec86637eab30f26921c
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-04 14:56:24 -0500

    working on intern test file

---
+++ __tests__/intern.test.js
@@ -0,0 +1,14 @@
+const { test, expect } = require("@jest/globals");
+const intern = require("../lib/intern");
+
+test("Can set school via constructor", () => {
+    const testValue = "UIC";
+    const e = new intern("Coffee", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"intern\"", () => {
+    const testValue = "intern";
+    const e = new intern("Coffee", 1, "test@test.com", "UIC");
+    expect(e.getRole()).toBe(testValue);
+});
