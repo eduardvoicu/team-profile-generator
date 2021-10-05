fe313adb498100719516937cce0c4f13ebe9f59c
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-05 07:19:30 -0500

    working on index js

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(_dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const { inherits } = require("util");
+const { finished } = require("stream");
+
+const employees = [];
+
+// Basic Questions
+
