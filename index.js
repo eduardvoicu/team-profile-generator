54c1a7d7c6c6048b2f29f344b475c9c94d86e1d4
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-05 12:35:44 -0500

    finished questions for intern

--- index.js
+++ index.js
@@ -85,3 +85,26 @@ const engineerQuestions = [
 ];
 
 // Intern Questions
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's email?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school"
+    }
+];
